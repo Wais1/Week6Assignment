36c92ed52f85ceabd5db5112dc3046b2c285c564
Wais1 <waisibrahim77@gmail.com>
2022-01-20 20:48:17 +0700

    portfolio work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Button, makeStyles } from "@material-ui/core";
+import { Person } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme)=>({
+  button: {
+    color:"white",
+    backgroundColor: theme.palette.success.dark,
+  }
+}))
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const classes = useStyles()
+  return <Button variant='contained' size='large' startIcon={<Person />} className={classes.button}>Hello world!</Button>;
 };
 
 export default App;
